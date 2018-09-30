3f652d7aaa0ce99efb767114ec119a3c23e3051b
Jason J. Yu <jjyu@scs.ryerson.ca>
2018-09-30 14:09:15 -0400

    updated download script to have manual download from gdrive

--- model_download_scripts/photometric_smoothness/download_model_weights.sh
+++ model_download_scripts/photometric_smoothness/download_model_weights.sh
@@ -1,6 +1,8 @@
 # download weights
 echo "Downloading weights..." 
-wget -O weights.tar.gz https://doc-0k-5o-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/1q5snsmj588h2s8eh09lc9d6qiltvspu/1538179200000/05082393995079092114/*/18MXPbjXUnGG_Fcij8YUs6mtyQcBwnSru?e=download
+#wget -O weights.tar.gz https://doc-0k-5o-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/1q5snsmj588h2s8eh09lc9d6qiltvspu/1538179200000/05082393995079092114/*/18MXPbjXUnGG_Fcij8YUs6mtyQcBwnSru?e=download
+echo "Please download this file and place it in this directory: https://drive.google.com/file/d/18MXPbjXUnGG_Fcij8YUs6mtyQcBwnSru/view?usp=sharing"
+read -p "Press to continue once weights.tar.gz is in this directory"
 # ask if we want to automatically copy weights and params into src
 while true; do
     read -p "Do you want the weights and hyper-parameters copied to src? (will overwrite)" yn
